6265b4ad26a997b78adfb2563bc5b4427f542404
elisa-morales <morales.elisa@yahoo.it>
2023-05-31 16:02:33 +0200

    about section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,14 @@
-const hamburger = document.querySelector(".hamburger")
-const navbar = document.querySelector(".nav-bar")
+const initApp = () => {
+  const hamburger = document.querySelector(".hamburger")
+  const navbar = document.querySelector(".nav-bar")
 
-function toggle() {
-  navbar.classList.toggle("active")
-  hamburger.classList.toggle("active")
-}
-
-hamburger.addEventListener("click", toggle)
-navbar.addEventListener("click", toggle)
-
-/*const initApp = () => {
-  const hamburgerBtn = document.getElementById("hamburger-button")
-  const mobileMenu = document.getElementById("mobile-menu")
-
-  const toggleMenu = () => {
-    mobileMenu.classList.toggle("hidden")
-    mobileMenu.classList.toggle("flex")
-    hamburgerBtn.classList.toggle("toggle-btn")
+  function toggle() {
+    hamburger.classList.toggle("active")
+    navbar.classList.toggle("active")
   }
 
-  hamburgerBtn.addEventListener("click", toggleMenu)
-  mobileMenu.addEventListener("click", toggleMenu)
+  hamburger.addEventListener("click", toggle)
+  navbar.addEventListener("click", toggle)
 }
 
 document.addEventListener("DOMContentLoaded", initApp)
-*/
