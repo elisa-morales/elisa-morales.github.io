bb72f22d017e011a5aae4c6cfe28a8a143f2b8b3
elisa-morales <morales.elisa@yahoo.it>
2023-08-05 12:14:10 +0200

    update main.js

---
+++ assets/js/data.js
@@ -0,0 +1,73 @@
+export const projects = [
+  {
+    title: "Hot Problems",
+    description: "Global warming dashboard",
+    link: "https://hot-problems.netlify.app/",
+    repository: "https://github.com/elisa-morales/hot-problems",
+    image: "assets/img/project-hot-problems.png",
+    alt: "climate change dashboard powered by Global Warming API",
+  },
+
+  {
+    title: "Eat your Greens!",
+    description: "Vegetarian recipe site",
+    link: "https://eatyourgreens.netlify.app/",
+    repository: "https://github.com/elisa-morales/eat-your-greens",
+    image: "assets/img/project-eat-your-greens.png",
+    alt: "vegetarian recipe site powered by Spoonacular API",
+  },
+
+  {
+    title: "Quizzical",
+    description: "React app trivia game",
+    link: "https://art-quizzical.netlify.app/",
+    repository: "https://github.com/elisa-morales/quizzical",
+    image: "assets/img/project-quizzical.png",
+    alt: "Quizzical: a React web-app trivia game",
+  },
+
+  {
+    title: "Spilling the Tech",
+    description: "News app from Hacker News API",
+    link: "https://spilling-the-tech.netlify.app/",
+    repository: "https://github.com/elisa-morales/spilling-the-tech",
+    image: "assets/img/project-spilling-the-tech.png",
+    alt: "Spilling the Tech: a news app from Hacker News API",
+  },
+
+  {
+    title: "We are the Champions",
+    description: "Messaging mobile app + Firebase",
+    link: "https://watc-soloproject.netlify.app/",
+    repository: "https://github.com/elisa-morales/we-are-the-champions",
+    image: "assets/img/project-watc.png",
+    alt: "Final project for the Build a Mobile App with Firebase course",
+  },
+
+  {
+    title: "Color palette generator",
+    description: "Website powered by The Color API",
+    link: "https://palette-generator-soloproject.netlify.app/",
+    repository: "https://github.com/elisa-morales/color-palette-generator",
+    image: "assets/img/project-palette-generator.png",
+    alt: "Color scheme generator built with The Color API",
+  },
+
+  {
+    title: "Coloriamo la rabbia",
+    description: "JavaScript counter app",
+    link: "https://rabbia-counter.netlify.app/",
+    repository: "https://github.com/elisa-morales/coloriamo-la-rabbia",
+    image: "assets/img/project-counter.png",
+    alt: "JavaScript counter app",
+  },
+
+  {
+    title: "First portfolio",
+    description: "Responsive single page portfolio",
+    link: "https://elisa-morales-dev.netlify.app/",
+    repository: "https://github.com/elisa-morales/first-portfolio",
+    image: "assets/img/project-portfolio.png",
+    alt: "Responsive single page HTML/CSS portfolio",
+  },
+]
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,18 @@
+import { projects } from "./data.js"
+
+const portfolio = document.getElementById("portfolio")
+
 const tabs = document.querySelectorAll("[data-target]")
 const tabContents = document.querySelectorAll("[data-content]")
-
 const themeButton = document.getElementById("theme-button")
 const darkTheme = "dark-theme"
 const iconTheme = "bx-sun"
 const selectedTheme = localStorage.getItem("selected-theme")
 const selectedIcon = localStorage.getItem("selected-icon")
-const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light")
-const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun")
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light"
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun"
 
 const scroll = ScrollReveal({
   origin: "top",
@@ -33,8 +38,12 @@ tabs.forEach((tab) => {
 })
 
 if (selectedTheme) {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme)
-  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  )
+  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
+    iconTheme
+  )
 }
 
 themeButton.addEventListener("click", () => {
@@ -50,3 +59,37 @@ scroll.reveal(`.profile-profession`, { delay: 600 })
 scroll.reveal(`.profile-social`, { delay: 700 })
 scroll.reveal(`.filters-content`, { delay: 800 })
 scroll.reveal(`.filters`, { delay: 900 })
+
+function getPortfolio() {
+  let portfolioHtml = ""
+  projects.map(
+    ({ title, description, link, repository, image, alt }) =>
+      (portfolioHtml += `
+      <article class="portfolio-card">
+              <img src=${image} alt=${alt}" class="portfolio-img" />
+
+              <div class="portfolio-modal">
+                <div>
+                  <span class="portfolio-sub">${description}</span>
+                  <h3 class="portfolio-title">${title}</h3>
+                  <div class="portfolio-links">
+                  <a href=${link} target="_blank" class="portfolio-button button button-small">
+                    <i class="bx bx-link-alt"></i>
+                  </a>
+                  <a href=${repository} target="_blank" class="portfolio-button button button-small">
+                    <i class='bx bxl-github'></i>
+                  </a>
+                </div>
+                </div>
+              </div>
+            </article>
+      `)
+  )
+  return portfolioHtml
+}
+
+function render() {
+  portfolio.innerHTML = getPortfolio()
+}
+
+render()
